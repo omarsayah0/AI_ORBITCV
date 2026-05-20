092d73086e76f8ad4948aca0ad14f9ff3bc3cc63
omar al ethamat <omaralethamat@gmail.com>
2026-05-20 23:47:20 +0300

    Adding ORB

--- includes/image.hpp
+++ includes/image.hpp
@@ -20,6 +20,7 @@ cv::Mat	sharpen_image(const cv::Mat& image);
 cv::Mat	sobel_edge_image(const cv::Mat& image);
 cv::Mat	laplacian_edge_image(const cv::Mat& image);
 
+//sift
 bool load_images( const std::string& path1,
      const std::string& path2, cv::Mat& img1, cv::Mat& img2);
 
@@ -32,7 +33,7 @@ bool find_good_matches(const cv::Mat& desc1, const cv::Mat& desc2,
      std::vector<cv::DMatch>& good_matches);
 
 bool compute_homography(const std::vector<cv::KeyPoint>& kp1, const std::vector<cv::KeyPoint>& kp2,
-    const std::vector<cv::DMatch>& good_matches, cv::Mat& homography);
+    const std::vector<cv::DMatch>& good_matches, cv::Mat& homography, cv::Mat& mask);
 
 void align_image(const cv::Mat& img2, const cv::Mat& homography,
      const cv::Size& output_size, cv::Mat& aligned);
@@ -43,5 +44,25 @@ void show_results(const cv::Mat& img1, const cv::Mat& img2,
 
 void sift_align_images();
 
+
+
+//orb
+bool compute_orb_features(const cv::Mat& gray,
+    std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors);
+
+bool find_good_orb_matches(const cv::Mat& desc1,
+    const cv::Mat& desc2, std::vector<cv::DMatch>& good_matches);
+
+void display_orb_results(const cv::Mat& img1, const cv::Mat& img2,
+    const std::vector<cv::KeyPoint>& kp1, const std::vector<cv::KeyPoint>& kp2,
+    const std::vector<cv::DMatch>& good_matches, const cv::Mat& aligned);
+
+bool load_images_orb(cv::Mat& img1, cv::Mat& img2);
+
+bool orb_align_images(void);
+
+bool compute_orb_homography(const cv::Mat& img1, const cv::Mat& img2,
+    std::vector<cv::KeyPoint>& kp1, std::vector<cv::KeyPoint>& kp2,
+    std::vector<cv::DMatch>& good_matches, cv::Mat& homography, cv::Mat& mask);
 
 #endif
